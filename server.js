48202c85a2e02cca6d947d36008b5ce5ebfd410f
gocityvibes <pat@713-shutter.com>
2025-07-11 11:16:21 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.post('/events', async (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});`);
+});
 }) => {
   console.log(`Server is running on port ${port}`);
 });
